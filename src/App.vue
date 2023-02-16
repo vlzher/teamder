baa2d2d127b5f583ca3db134f635e73ebc203047
Władysław <89138444+vlzher@users.noreply.github.com>
2023-02-16 22:43:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header-section></header-section>
+  <div class="centered">
+    <create-section></create-section>
+    <search-bar/>
+  </div>
+
+</template>
+
+
+<script lang="ts">
+
+import HeaderSection from "@/components/MainPage/HeaderSection.vue";
+import CreateSection from "@/components/MainPage/CreateSection.vue";
+import SearchBar from "@/components/MainPage/SearchBar.vue";
+export default {
+  name:"App",
+  components:{
+    HeaderSection,
+    CreateSection,
+    SearchBar,
+  }
+}
+</script>
+
+<style scoped>
+  .centered{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
+
+
