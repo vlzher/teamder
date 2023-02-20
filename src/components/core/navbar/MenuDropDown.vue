b1911c36f01a76a16249ae90265c6836957026c3
Władysław <89138444+vlzher@users.noreply.github.com>
2023-02-20 21:20:16 +0100

    header fixes

--- src/components/core/navbar/MenuDropDown.vue
+++ src/components/core/navbar/MenuDropDown.vue
@@ -10,7 +10,7 @@
     >
       <span class="sr-only">Open user menu</span>
       <div
-        class="w-8 h-8 rounded-full bg-cover bg-[url('../assets/avatar.png')]"
+        class="w-10 h-10 rounded-full bg-cover bg-[url('../assets/avatar.png')]"
       ></div>
     </button>
     <div
@@ -52,27 +52,5 @@
         </li>
       </ul>
     </div>
-    <button
-      data-collapse-toggle="mobile-menu-2"
-      type="button"
-      class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      aria-controls="mobile-menu-2"
-      aria-expanded="false"
-    >
-      <span class="sr-only">Open main menu</span>
-      <svg
-        class="w-6 h-6"
-        aria-hidden="true"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-          clip-rule="evenodd"
-        ></path>
-      </svg>
-    </button>
   </div>
 </template>
